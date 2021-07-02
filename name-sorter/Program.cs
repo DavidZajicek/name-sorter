743347ec3d88173ff2824a3a4f56001668f5f8ad
David Zajicek <d.zajicek7@gmail.com>
2021-07-02 10:01:15 +1000

    Initial Commit Working Person class with GivenName and Surname with a ToString override to Print the full name.
    Test List manually created
    
    Working sorter using LINQ OrderBy().ThenBy()
    
    Test PrinArrayIndexAndValues(list) to visually see index and values when printing to ensure list order before and after sorting.Add project files.

---
+++ name-sorter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace name_sorter
+{
+    public class Person
+    {
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+
+        public override string ToString()
+        {
+            return GivenName + " " + Surname;
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare people List
+            List<Person> people = new List<Person>();
+            // Add people to people List
+            people.Add(new Person() { GivenName = "Abdul Jafar", Surname = "Vader" });
+            people.Add(new Person() { GivenName = "Adbul Jafar", Surname = "Vader" });
+            people.Add(new Person() { GivenName = "Abdul Jafar", Surname = "Cader" });
+
+            Console.WriteLine("List of names before sorting:");
+            PrintArrayIndexAndValues(people);
+            // Using LINQ to compare strings within list by sorting by surname, and then given name
+            IEnumerable<Person> sortedPeople = people.OrderBy(person => person.Surname).ThenBy(person => person.GivenName);
+            Console.WriteLine("List of names after a default sort: (Surname only)");
+            PrintArrayIndexAndValues(sortedPeople);
+        }
+
+        public static void PrintArrayIndexAndValues(IEnumerable list)
+        {
+            int i = 0;
+            foreach (var item in list)
+            {
+                Console.WriteLine($"[{i++}]:   {item}");
+            }
+        }
+    }
+
+
+}
