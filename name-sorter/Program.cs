676e468ea9b65554e992e5488064846fd92e0c3e
David Zajicek <d.zajicek7@gmail.com>
2021-07-02 12:03:10 +1000

    Revert "Added "ignore empty lines" to file reader and Travis CI to ignore mono"
    This reverts commit 3c4e2d71890f7ba149c9aa32fef38564bd9249fe.

--- name-sorter/Program.cs
+++ name-sorter/Program.cs
@@ -23,17 +23,16 @@ namespace name_sorter
             // Declare people List
             List<Person> people = new List<Person>();
 
+            //int counter = 0;
             string line;
             
             // Read the file and display it line by line
             StreamReader file = new StreamReader(@".\unsorted-names-list.txt");
             while((line = file.ReadLine()) != null)
             {
-                if(line != "") // Ignore empty lines
-                {
-                    var names = line.Split();
-                    people.Add(new Person() { GivenName = String.Join(" ", names, 0, (names.Length - 1)), Surname = names[names.Length-1] });
-                }
+                var names = line.Split();
+                people.Add(new Person() { GivenName = String.Join(" ", names, 0, (names.Length - 1)), Surname = names[names.Length-1] });
+                //counter++;
             }
 
 
